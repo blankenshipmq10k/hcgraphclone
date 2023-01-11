3a178d9b8a532fa16b35dc2cec48da10fbed49af
Max Blankenship <blankenshipmq@gmail.com>
2023-01-11 12:57:41 -0600

    moving back to context...

--- hcgraph/Program.cs
+++ hcgraph/Program.cs
@@ -44,10 +44,10 @@ builder.Services
 
 var app = builder.Build();
 
-using (var scope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
-{
-    scope.ServiceProvider.GetRequiredService<SampleDbContext>().Database.Migrate();
-}
+// using (var scope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+// {
+//     scope.ServiceProvider.GetRequiredService<SampleDbContext>().Database.Migrate();
+// }
 
 app.MapGraphQL();
 
